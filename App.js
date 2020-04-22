0d7e3436298cbb016a7c2b7448e4413ef0c5836c
conor Watson <conor.watson@live.com>
2020-04-22 21:36:16 +0100

    initial Commit

--- App.js
+++ App.js
@@ -1,36 +1,52 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import { PanResponder, Platform, StyleSheet, Text, Image, View, Animated } from 'react-native';
+import Draggable from 'react-native-draggable';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+      <DraggableUnitCard unitImage={CardImages.units.ObiWan}/>
+      <DraggableUnitCard unitImage={CardImages.units.Phase1CloneTroopers}/>
+      <DraggableUnitCard unitImage={CardImages.units.Phase1CloneTroopers}/>
+      <DraggableUnitCard unitImage={CardImages.units.Phase2CloneTroopers}/>
+      <DraggableUnitCard unitImage={CardImages.units.BarcSpeeder}/>
+      <DraggableUnitCard unitImage={CardImages.units.TX130SaberClassFighterTank}/>
     </View>
   );
 }
 
+function DraggableUnitCard(props){
+  return (
+    <View style={{width: 418, height: 300, marginBottom: 10}}>
+      <Draggable>
+        <Image style={{width: 418, height: 300}} source={props.unitImage} />  
+      </Draggable>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1, //makes it fit the whole screen
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    backgroundColor: 'black',
+    flexWrap: 'wrap'
+  }
 });
+
+const CardImages = {
+  units: {
+    DarthVader: require('./img/DarthVader.png'),
+    ZSpeederBikes: require('./img/74-ZSpeederBikes.png'),
+    ATST: require('./img/AT-ST.jpg'),
+    ObiWan: require('./img/Obi-WanKenobi.png'),
+    Phase1CloneTroopers: require('./img/PhaseICloneTroopers.png'),
+    Phase2CloneTroopers: require('./img/PhaseIICloneTroopers.png'),
+    BarcSpeeder: require('./img/BARCSpeeder.png'),
+    TX130SaberClassFighterTank: require('./img/TX-130Saber-ClassFighterTank.png'),
+  },
+  upgrades: {
+    ForcePush: require('./img/upgrades/force/ForcePush.png')
+  }
+}
